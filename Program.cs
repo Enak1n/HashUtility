004698afd615ecf42acf227a9720613707d6a546
Enak1n <masj963@gmail.com>
2023-12-15 19:02:33 +0700

    Remove cycle

--- Program.cs
+++ Program.cs
@@ -1,52 +1,26 @@
 ﻿using HashUtility;
+using Microsoft.Extensions.Configuration;
+using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        string password = null;
-        string salt = null;
-        bool use3Des = false;
+        var configuration = new ConfigurationBuilder()
+                   .AddCommandLine(args, new Dictionary<string, string>
+                   {
+                       { "-p", "p" },
+                       { "-s", "s" }})
+                   .Build();
 
-        if(args.Length == 0)
+        string password = configuration["p"];
+        string salt = configuration["s"];
+        bool use3Des = args.Contains("-d");
+
+        if (string.IsNullOrEmpty(password) || string.IsNullOrEmpty(salt))
         {
             Console.WriteLine("Проверьте правильность введения команды!");
             return;
-        }
-
-        for (int i = 0; i < args.Length; i++)
-        {
-            switch (args[i])
-            {
-                case "-p":
-                    try
-                    {
-                        password = args[++i];
-                    }
-                    catch
-                    {
-                        Console.WriteLine("Проверьте правильность введения команды!");
-                        return;
-                    }
-                    break;
-                case "-s":
-                    try
-                    {
-                        salt = args[++i];
-                    }
-                    catch
-                    {
-                        Console.WriteLine("Проверьте правильность введения команды!");
-                        return;
-                    }
-                    break;
-                case "-d":
-                    use3Des = true;
-                    break;
-                default:
-                    Console.WriteLine("Проверьте правильность введения команды!");
-                    return;
-            }
         }
 
         var hashedPassword = PasswordHasher.Hash(password, salt, use3Des);
